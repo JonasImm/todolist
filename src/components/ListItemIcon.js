3efe53911a9bd16e47aca6e213d07aa5597e045c
JonasImm <jonas.imm@gmail.com>
2020-08-18 14:54:21 +0200

    Add bullet point

---
+++ src/components/ListItemIcon.js
@@ -0,0 +1,8 @@
+import React from "react";
+import buttonSrc from "../assets/button.svg";
+
+const ListItemIcon = () => {
+  return <img src={buttonSrc} alt="Button" />;
+};
+
+export default ListItemIcon;
