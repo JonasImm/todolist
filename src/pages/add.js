9c61f69e509757870f5f8a3a44d4ab6ea7266311
JonasImm <jonas.imm@gmail.com>
2020-08-19 11:49:00 +0200

    Form data send to object, on every change

--- src/pages/add.js
+++ src/pages/add.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { createTodo } from "../api/todos";
 
 const Add = () => {
+  const [title, setTitle] = useState(null);
+  const [date, setDate] = useState(null);
+
+  function titleChange(title) {
+    setTitle(title.target.value);
+  }
+
+  function dateChange(date) {
+    setDate(date.target.value);
+  }
+
   function addTodo() {
-    createTodo("Hello", "20.10.2021");
+    createTodo(title, date);
   }
   return (
-    <div>
-      <form>
-        <label>
-          Task:
-          <input type="text" name="Task" />
-        </label>
-        <label>
-          Date:
-          <input type="date" name="Date" />
-        </label>
-        <input type="submit" value="Add task" onClick={addTodo()} />
-      </form>
-    </div>
+    <form>
+      <label>
+        Title:
+        <input type="text" name="Title" onChange={titleChange} />
+      </label>
+      <label>
+        Date:
+        <input type="date" name="Date" onChange={dateChange} />
+      </label>
+      <input type="submit" value="Add task" onSubmit={addTodo()} />
+    </form>
   );
 };
 
